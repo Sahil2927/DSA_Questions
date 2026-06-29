88dc7ddacb617a1c965e52f1632bfa803f93a055
Sahil2927 <157943612+Sahil2927@users.noreply.github.com>
2026-06-29 12:48:30 +0530

    Time: 6 ms (27.28%), Space: 14.2 MB (45.15%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>>result;
+    int n;
+    void solve(vector<int>&temp,vector<int>& candidates, int target,int idx,int sum){
+        // Base case 1: Found a valid combination
+        if (sum == target) {
+            result.push_back(temp);
+            return;
+        }
+        
+        // Base case 2: Exceeded target or reached end of array
+        if (sum > target || idx >= n) {
+            return;
+        }
+        temp.push_back(candidates[idx]);
+        solve(temp,candidates,target,idx,sum+candidates[idx]);
+        temp.pop_back();
+        solve(temp,candidates,target,idx+1,sum);
+    }
+
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int>temp;
+        n=candidates.size();
+        solve(temp,candidates,target,0,0);
+        return result;
+    }
+};
