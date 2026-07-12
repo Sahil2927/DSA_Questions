f11a16121590d506925feed7c4e21c0196df590e
Sahil2927 <157943612+Sahil2927@users.noreply.github.com>
2026-07-13 02:56:50 +0530

    Time: 5 ms (0.02%), Space: 10.8 MB (66.13%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -9,17 +9,66 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
-    void inorder(TreeNode* root,vector<int>&result){
-        if(root==NULL) return;
-        inorder(root->left,result);
-        result.push_back(root->val);
-        inorder(root->right,result);
-    }
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>result;
-        inorder(root,result);
-        return result;
+// class Solution {
+// public:
+//     void inorder(TreeNode* root,vector<int> &result){
+//         if(root==nullptr) return;
+//         inorder(root->left,result);
+//         result.push_back(root->val);
+//         inorder(root->right,result);
+//     }
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         vector<int> result;
+//         inorder(root,result);
+//         return result;
+//     }
+// };
+class Solution{
+    public:
+    vector<int> inorderTraversal(TreeNode* root){
+        // stack <TreeNode*> st;
+        // TreeNode* node=root;
+        // vector<int> inorder;
+        // while(true){
+        //     if(node!=NULL){
+        //         st.push(node);
+        //         node=node->left;
+        //     }
+        //     else{
+        //         if(st.empty()==true) break;
+        //         node=st.top();
+        //         st.pop();
+        //         inorder.push_back(node->val);
+        //         node=node->right;
+        //     }      
+        // }
+
+        // Morris Traversal 
+        // TC=O(N)
+        // SC=O(1)
+        vector<int>inorder;
+        TreeNode* cur=root;
+        while(cur!=NULL){
+            if(cur->left==NULL){
+                inorder.push_back(cur->val);
+                cur=cur->right;
+            }
+            else{
+                TreeNode* prev=cur->left;
+                while(prev->right && prev->right!=cur){
+                    prev=prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right=cur;
+                    cur=cur->left;
+                }
+                else{
+                    prev->right=NULL;
+                    inorder.push_back(cur->val);
+                    cur=cur->right;
+                }
+            }
+        }
+        return inorder;
     }
 };
