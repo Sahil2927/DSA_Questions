33b6bd76861c02c1b0eb3ac72d50badbf929f541
Sahil2927 <157943612+Sahil2927@users.noreply.github.com>
2026-07-18 12:51:52 +0530

    Time: 7 ms (11.82%), Space: 23.3 MB (5.56%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,12 +1,52 @@
+// class Solution {
+// public:
+//     void DFS(vector<vector<int>>&adj,int u,vector<bool>&visited){
+//         visited[u]=true;
+//         for(int &v:adj[u]){
+//             if(!visited[v]){
+//                 DFS(adj,v,visited);
+//             }
+//         }
+//     }
+//     int findCircleNum(vector<vector<int>>& isConnected) {
+//         int V=isConnected.size();
+//         vector<vector<int>>adj(V);
+//         for(int i=0;i<V;i++){
+//             for(int j=0;j<V;j++){
+//                 if(isConnected[i][j]==1 && i!=j){
+//                     adj[i].push_back(j);
+//                     adj[j].push_back(i);
+//                 }
+//             }
+//         }
+//         vector<bool>visited(V,false);
+//         int count=0;
+//         for(int i=0;i<V;i++){
+//             if(!visited[i]){
+//                 count++;
+//                 DFS(adj,i,visited);
+//             }
+//         }
+//         return count;
+//     }
+// };
+
 class Solution {
 public:
-    void DFS(vector<vector<int>>&adj,int u,vector<bool>&visited){
+    void BFS(vector<vector<int>>&adj,int u,vector<bool>&visited){
         visited[u]=true;
-        for(int &v:adj[u]){
-            if(!visited[v]){
-                DFS(adj,v,visited);
+        queue<int>q;
+        q.push(u);
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            for(int &v:adj[u]){
+                if(!visited[v]){
+                    BFS(adj,v,visited);
+                }
             }
         }
+        
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int V=isConnected.size();
@@ -24,7 +64,7 @@ public:
         for(int i=0;i<V;i++){
             if(!visited[i]){
                 count++;
-                DFS(adj,i,visited);
+                BFS(adj,i,visited);
             }
         }
         return count;
